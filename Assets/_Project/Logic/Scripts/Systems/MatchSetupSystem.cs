2fbbeebf189952230b73dd972a45fcf8c825a1ac
Redream <mr.golyadkina@mail.ru>
2025-07-14 16:40:32 +0300

    added match setup system

--- Assets/_Project/Logic/Scripts/Systems/MatchSetupSystem.cs
+++ Assets/_Project/Logic/Scripts/Systems/MatchSetupSystem.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestSystem : MonoBehaviour
+public class MatchSetupSystem : MonoBehaviour
 {
     [SerializeField] private List<CardData> _deckData;
 
     private void Start()
     {
         CardSystem.Instance.Setup(_deckData);
+        DrawCardsGA drawCardsGA = new(5);
+        ActionSystem.Instance.Perform(drawCardsGA);
     }
 }
