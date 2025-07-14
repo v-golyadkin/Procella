d143a0619d706ef43eb7a40b3b2f4900372b35a5
Redream <mr.golyadkina@mail.ru>
2025-07-14 13:03:25 +0300

    added action system

---
+++ Assets/_Project/Logic/Scripts/General/Action System/GameAction.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+public abstract class GameAction
+{
+    public List<GameAction> PreReactions { get; private set; } = new();
+    public List<GameAction> PerformReactions { get; private set; } = new();
+    public List<GameAction> PostReactions { get; private set; } = new();
+}
