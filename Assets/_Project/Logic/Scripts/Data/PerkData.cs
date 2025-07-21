9577c56ddf2c00792ebf5d52959f6043537b5c95
Redream <mr.golyadkina@mail.ru>
2025-07-21 16:51:58 +0300

    Update PerkData.cs

--- Assets/_Project/Logic/Scripts/Data/PerkData.cs
+++ Assets/_Project/Logic/Scripts/Data/PerkData.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PerkData : ScriptableObject
 {
     [field: SerializeField] public Sprite Image { get; private set; }
-    [field: SerializeReference, SR] public PerkCondition PercCondition { get; private set; }
+    [field: SerializeReference, SR] public PerkCondition PerkCondition { get; private set; }
     [field: SerializeReference, SR] public AutoTargetEffect AutoTargetEffect { get; private set; }
     [field: SerializeField] public bool UseAutoTarget { get; private set; } = true;
     [field: SerializeField] public bool UseActionCasterAsTarget { get; private set; } = false;
