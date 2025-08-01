7d1ea615f59004b0a6b1944685874a0cd5aeaa67
Redream <mr.golyadkina@mail.ru>
2025-08-01 13:41:18 +0300

    fix bug with null reference exception

--- Assets/_Project/Logic/Scripts/General/Util/MouseUtil.cs
+++ Assets/_Project/Logic/Scripts/General/Util/MouseUtil.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public static class MouseUtil
 {
-    private static Camera _camera = Camera.main;
+    private static Camera _camera;
 
     public static Vector3 GetMousePositionInWorldSpace(float zValue = 0f)
     {
+        _camera = GetCamera();
+        if( _camera == null )
+        {
+            return Vector3.zero;
+        }
+
         Plane dragPlane = new(_camera.transform.forward, new Vector3(0, 0, zValue));
         Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
         if(dragPlane.Raycast(ray, out float distance))
@@ -14,5 +20,16 @@ public static class MouseUtil
         }
 
         return Vector3.zero;
+    }
+
+    private static Camera GetCamera()
+    {
+        if(_camera != null && _camera.gameObject != null)
+        {
+            return _camera;
+        }
+
+        _camera = Camera.main;
+        return _camera;
     }
 }
