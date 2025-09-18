9c7b4d6a21edf138637a6b9a61029708ccabfcc6
Redream <mr.golyadkina@mail.ru>
2025-09-18 10:44:25 +0300

    Update LevelData.cs

--- Assets/_Project/Logic/Scripts/Data/LevelData.cs
+++ Assets/_Project/Logic/Scripts/Data/LevelData.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data/Level", fileName = "Level")]
 public class LevelData : ScriptableObject
 {
-    [field: SerializeField] public EnemyData LeftEnemy { get; private set; }
-    [field: SerializeField] public EnemyData MiddleEnemy { get; private set; }
-    [field: SerializeField] public EnemyData RightEnemy { get; private set; }
-
     [field: SerializeField] public List<EnemyData> Enemies { get; private set; }
 }
+
